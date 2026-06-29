92a6457ffc35b3121c6081a1f8ca3237c43a142a
Hemant Mandge <mandge.hemant@gmail.com>
2026-06-29 22:07:21 +0530

    Create 28-medium-Compressed Mode.sql

---
+++ datalemur/28-medium-Compressed Mode.sql
@@ -0,0 +1,34 @@
+-- ============================================================
+-- PLATFORM  : Datalemur
+-- PROBLEM   : Compressed Mode
+-- DIFFICULTY: medium
+-- URL       : https://datalemur.com/questions/alibaba-compressed-mode
+-- DATE      : 29 June 2026
+-- ============================================================
+-- PROBLEM STATEMENT:
+
+	/* 
+		You're given a table containing the item count for each order on Alibaba, 
+        along with the frequency of orders that have the same item count. 
+        Write a query to retrieve the mode of the order occurrences. Additionally, 
+        if there are multiple item counts with the same mode, the results should be sorted in ascending order.
+
+		Clarifications:
+
+	    item_count: Represents the number of items sold in each order.
+		order_occurrences: Represents the frequency of orders with the corresponding number of items sold per order.
+		For example, if there are 800 orders with 3 items sold in each order, 
+        the record would have an item_count of 3 and an order_occurrences of 800.
+     */
+
+-- TECHNIQUE USED:
+-- subquery
+
+-- TIME TAKEN: 05 minutes (Took hints)
+-- ============================================================
+
+SELECT item_count
+FROM items_per_order
+WHERE 
+  order_occurrences = (SELECT MAX(order_occurrences) FROM items_per_order)
+;
